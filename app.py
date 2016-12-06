a2959b5b5e63839baf391583e42d9037db2357d4
Suraj Pai <surajballambat@gmail.com>
2016-12-06 17:05:19 +0530

    updated HTTP methods

--- app.py
+++ app.py
@@ -7,20 +7,19 @@ import os
 app=Flask(__name__)
 
 
-@app.route("/webhook",methods='POST')
+@app.route("/webhook",methods=['GET','POST'])
 def webhook():
-    if (request.method=='POST'):
-        data=request.json
-        result=data["result"]
-        action=result["action"]
-        channel=result["parameters"]["channel"]
-        show=tvlisting(channel)
-        obj={}
-        obj["speech"]="The TV show playing right now is"+show
-        obj["data"]={"facebook": {"TV show playing now is "+ show}}
+    data=request.get_json()
+    result=data["result"]
+    action=result["action"]
+    channel=result["parameters"]["channel"]
+    show=tvlisting(channel)
+    obj={}
+    obj["speech"]="The TV show playing right now is"+show
+    obj["data"]={"facebook": {"TV show playing now is "+ show}}
 
-        response=json.dumps(obj)
-        return response
+    response=json.dumps(obj)
+    return response
 
 
 @app.route("/")
