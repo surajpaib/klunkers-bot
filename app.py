74a8fab3f4078e1610d5ae516cf05e520218abb9
Suraj Pai <surajballambat@gmail.com>
2016-12-06 17:15:37 +0530

    Updated GET POST

--- app.py
+++ app.py
@@ -9,17 +9,20 @@ app=Flask(__name__)
 
 @app.route("/webhook",methods=['GET','POST'])
 def webhook():
-    data=request.get_json()
-    result=data["result"]
-    action=result["action"]
-    channel=result["parameters"]["channel"]
-    show=tvlisting(channel)
-    obj={}
-    obj["speech"]="The TV show playing right now is"+show
-    obj["data"]={"facebook": {"TV show playing now is "+ show}}
+    if (request.method=='POST'):
+        data=request.get_json()
+        result=data["result"]
+        action=result["action"]
+        channel=result["parameters"]["channel"]
+        show=tvlisting(channel)
+        obj={}
+        obj["speech"]="The TV show playing right now is"+show
+        obj["data"]={"facebook": {"TV show playing now is "+ show}}
 
-    response=json.dumps(obj)
-    return response
+        response=json.dumps(obj)
+        return response
+    if (request.method=='GET'):
+        return "Page is online"
 
 
 @app.route("/")
