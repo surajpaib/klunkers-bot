03b4c4ec39e20d33cea61b39e405e50828bafb24
Suraj Pai <surajballambat@gmail.com>
2016-12-07 18:44:15 +0530

    updated dict

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ def webhook():
         channel=result["parameters"]["channel"]
         show=tvlisting(channel)
         obj={
-            'speech':"The TV show playing right now is"+show
-            'displayText':"The show that's playing right now is"
-            'data':{'facebook':{'TV show playing now is'+show}}
-            'contextOut': None
+            'speech':"The TV show playing right now is"+show,
+            'displayText':"The show that's playing right now is",
+            'data':{'facebook':{'TV show playing now is'+show}},
+            'contextOut': None,
             'source': None
 
         }
